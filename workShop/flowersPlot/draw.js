af9b5cd21d88c91cbada7623e50c972b7b935aab
listenzcc <listenzcc@mail.bnu.edu.cn>
2021-07-18 23:18:26 +0800

    Develop Update

--- workShop/flowersPlot/draw.js
+++ workShop/flowersPlot/draw.js
@@ -1,10 +1,12 @@
 // * Require flower.js
 console.log(d3.version)
 
-let width = 800
-let height = 800
-let padding = 100
-let svg = d3.select('#canvas').append('svg')
+let svg = d3.select('#mainSvg')
+
+let width = document.getElementById('mainSvg').getBoundingClientRect().width
+let height = document.getElementById('mainSvg').getBoundingClientRect().height
+let padding = Math.min(width, height) / 30
+
 let defs = svg.append('defs');
 // let colors = ['LightPink', 'Peru', 'HotPink', 'DeepPink', 'RosyBrown'];
 // let colors = cssColors
@@ -35,8 +37,8 @@ for (let c in cssColors) {
 
 /**
  * * Add color into radialGradient
- * @param {dom} defs 
- * @param {string} color 
+ * @param {dom} defs
+ * @param {string} color
  */
 let addColor = function (defs, color) {
     let id = 'radialGradient-crown-' + color
@@ -76,53 +78,67 @@ let randomTransform = function () {
     return context
 }
 
-for (let i = 0; i < 7; i++) {
-    let color = colors[d3.randomInt(colors.length)()]
-    let n_petals = d3.randomInt(4, 8)()
-    flower = generate_flower(n_petals, 100, 0.5, 0.2, 0.2, color)
+let layoutTransform = function (c, r) {
+    let context =
+        'translate('
+        + (padding + (width - padding) * c / 23)
+        + ', '
+        + (padding + (height - padding) * r / 31)
+        + ')'
+    return context
+}
 
-    // let transform =
-    let g = svg.append('g').attr('class', 'flower')
-        .attr('transform', randomTransform())
+// 23 Columns, 31 Rows
+for (let _c = 0; _c < 23; _c++) {
+    for (let _r = 0; _r < 31; _r++) {
+        let color = colors[d3.randomInt(colors.length)()]
+        let n_petals = d3.randomInt(4, 9)()
+        flower = generate_flower(n_petals * 1, 100 * 0.3, 0.5, 0.2 * 0.5, 0.2 * 0.5, color)
+        console.log(flower)
 
-    g.append('g')
-        .append('text')
-        .attr('style', 'fill: white')
-        .text(color + ', ' + n_petals)
+        // let transform =
+        let g = svg.append('g').attr('class', 'flower')
+            .attr('transform', layoutTransform(_c, _r))
 
-    g.append('g')
-        .append('path')
-        .attr('d', bezier(flower))
-        .attr('fill', 'url(#radialGradient-shadow)')
-        .attr('class', 'shadow')
+        g.append('g')
+            .append('text')
+            .attr('style', 'fill: white')
+            .text(color + ', ' + n_petals)
 
-    g.append('g')
-        .selectAll('path')
-        .data(flower)
-        .enter()
-        .append('path')
-        .attr('d', function (d) {
-            let xy = ang_rad_2_xy(d[0], d[1])
-            return 'M0 0 ' + xy[0] + ' ' + xy[1]
-        })
-        .attr('stroke', function (d, i) {
-            if (i % 2 === 0) {
-                return undefined
-            }
-            return 'gray'
-        })
-        .attr('class', 'lines')
-        .attr('opacity', 0.5)
+        g.append('g')
+            .append('path')
+            .attr('d', bezier(flower))
+            .attr('fill', 'url(#radialGradient-shadow)')
+            .attr('class', 'shadow')
 
-    let fillUrl = addColor(defs, flower.color)
-    g.append('g')
-        .selectAll('path')
-        .data([flower])
-        .enter()
-        .append('path')
-        .attr('d', area)
-        .attr('fill', fillUrl)
-        .attr('class', 'crown')
-        .attr('id', 'a#b')
+        g.append('g')
+            .selectAll('path')
+            .data(flower)
+            .enter()
+            .append('path')
+            .attr('d', function (d) {
+                let xy = ang_rad_2_xy(d[0], d[1])
+                return 'M0 0 ' + xy[0] + ' ' + xy[1]
+            })
+            .attr('stroke', function (d, i) {
+                if (i % 2 === 0) {
+                    return undefined
+                }
+                return 'gray'
+            })
+            .attr('class', 'lines')
+            .attr('opacity', 0.5)
 
+        let fillUrl = addColor(defs, flower.color)
+        g.append('g')
+            .selectAll('path')
+            .data([flower])
+            .enter()
+            .append('path')
+            .attr('d', area)
+            .attr('fill', fillUrl)
+            .attr('class', 'crown')
+            .attr('id', 'a#b')
+
+    }
 }
